da73cc434c81b7c6b58bf11e2d5517a86d8c7b9d
Nico Brinkkemper <nico.brinkkemper1@persgroep.net>
2021-10-01 13:19:55 +0200

    move tests to folder

--- src/lib/isArrayLike.spec.ts
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-
-import { isArrayLike } from './isArrayLike';
-
-test("Should return true because it's a list of tuple", (t) => {
-  const isTuple = isArrayLike(Array.isArray);
-  t.is(isTuple([[]]), true);
-});
