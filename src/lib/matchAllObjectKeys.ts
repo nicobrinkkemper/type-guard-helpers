58c25e34950704f51c9a739551279ff93cfed802
Nico Brinkkemper <Nico.Brinkkemper@dpgmedia.nl>
2021-11-14 17:22:56 +0100

    fix doc

--- src/lib/matchAllObjectKeys.ts
+++ src/lib/matchAllObjectKeys.ts
@@ -1,6 +1,10 @@
 import { matchKeyInObject } from './matchKeyInObject';
 import { negateGuard } from './negateGuard';
 
+/**
+ * Given a object, returns a Type Guard that takes an object and will check if the latter given object contains all the keys of the first given object.
+ * @category Type Guard Creator
+ */
 const matchAllObjectKeys = <
 	KeyedObject extends {
 		readonly [k in keyof KeyedObject]: unknown;
