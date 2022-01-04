8e447716eca9f89f95715a6f8785ea8cee76b1e8
Nico Brinkkemper <nico@brinkkemper.nl>
2022-01-04 15:14:20 +0100

    fix test

--- src/spec/guardRecord.spec.ts
+++ src/spec/guardRecord.spec.ts
@@ -8,17 +8,22 @@ const isTranslation = guardRecord(
 );
 
 test('Should return true for objects with translations', (t) => {
-	const fnObj = Object.assign((a: string) => a, {
+	const strObj = Object.assign('s', {
 		translation: 'hi',
-		nameSpace: 'yo',
+		nameSpace: 'str',
 	});
 	t.is(isTranslation({ translation: '' as unknown }), true);
 	t.is(isTranslation({ translation: 'true', someOtherThing: 'yes' }), true);
-	t.is(isTranslation(fnObj), true);
+	t.is(isTranslation(strObj), true);
 });
 
 test('Should return true for translations', (t) => {
+	const fnObj = Object.assign((a: string) => a, {
+		translation: 'hi',
+		nameSpace: 'fn',
+	});
 	t.is(isTranslation({ translation: true as unknown }), false);
 	t.is(isTranslation(false as unknown), false);
 	t.is(isTranslation(null as unknown), false);
+	t.is(isTranslation(fnObj), false);
 });
