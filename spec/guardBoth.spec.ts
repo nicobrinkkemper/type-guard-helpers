0fa763a2cc5c76fa2e0b6f6a873de30a65a0e78e
Nico Brinkkemper <nico@brinkkemper.nl>
2022-01-15 21:49:25 +0100

    add test guardBoth

---
+++ spec/guardBoth.spec.ts
@@ -0,0 +1,28 @@
+import test from 'ava';
+
+import { guardBoth, matchType } from '../src';
+
+import { guardArrayValues } from './guardArrayValues';
+
+const isString = matchType('string');
+test('Should return true for string', (t) => {
+	t.is(isString('fooBath'), true);
+});
+
+const startsWithFoo = guardBoth(isString, (val): val is `foo${string}` => {
+	return !!val.startsWith('foo');
+});
+
+const isFooArray = guardArrayValues(startsWithFoo);
+
+test('Should return true for things that start with foo', (t) => {
+	t.is(startsWithFoo('fooBath'), true);
+	t.is(startsWithFoo('fooDrink'), true);
+	t.is(isFooArray(['fooA', 'fooB', 'fooC']), true);
+});
+
+test('Should return false for anything else', (t) => {
+	t.is(isFooArray(['barf'] as unknown), false);
+	t.is(startsWithFoo(['oo'] as unknown), false);
+	t.is(startsWithFoo('bar'), false);
+});
